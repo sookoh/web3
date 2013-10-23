5e9460077f59cb991d2feed7e8366dd4b601831f
soo <kohs640@newschool.edu>
2013-10-23 01:51:05 -0400

    hw2

--- hw2/js/main.js
+++ hw2/js/main.js
@@ -6,17 +6,27 @@
  *
  */
 
-$('.feelings').click( function(event){  
+
+$('.select').click(function() {
+    console.log($(this).text());
+    displayMoodPost($(this).text());
+});
+
+function displayMoodPost(mood) {
+    $('article').hide();
+    $('article.' + mood).show();
+}
+
+
+$('.feelings').click(function(event){
 	event.preventDefault();
 	console.log('feeling click');
-	var myemo = $(this).attr('id')
+	var myemo = $(this).attr('id');
 	$('#currentemo').val(myemo);
 	console.log(myemo);
 });
 
-
 var posts = [];
-
 
 /**
  * form action
@@ -32,6 +42,9 @@ $('#save').click( function(event){
     var post = {};
     post.title = $('#currentemo').val();
     post.content = $('#content').val();
+    post.date = new Date();
+
+    
 	// set id 
 	// the id will be last id + 1
 
@@ -54,10 +67,16 @@ $('#save').click( function(event){
  */
 
 function displayPost(post){
-    
-   var html = '<article class="'+ post.title +'"><h2>'+ post.title +'</h2><p>'+ post.content +'</p></article>';
+ //  var html = '<article class="'+ post.title +'"><h2>'+ post.title +'</h2><p>'+ post.content +'</p></article>';
+  var html = '<article class="'+ post.title +'"><h2>'+ post.title +'</h2><p>'+ post.content +'</p><p class=mydate>'+ post.date +'</p></article>';
     $('#feed').prepend(html);
 }
+ /*
+    for(var i=0; i< 20; i++){
+   var html = '<article id="a'+[i] + '" class="'+ post.title +'"><h2>'+ post.title +'</h2><p>'+ post.content +'</p></article>';
+    $('#feed').prepend(html);
+    }
+    */
 
 /*
 $('#feed').click( function(){
@@ -69,18 +88,23 @@ $('section').on('click', 'article', function(){
 	//$(this).text('yaya');
 	//$('article').removeClass('article');
 	$(this).toggleClass('change');
+/*
+for (var i=0; i<20; i++){
+    $('#a'+[i]).toggleClass('change');
+*/
+	
 	//$(this).css('color','#0f0');
 	
 	console.log('hi soooo');
 });
 
-
+/*
 $('#happy').on('click', 'article', function(){  
 	//$(this).load('happy.html');
 	$(this).text('yaya');
 	console.log('sooo happy');
 });
-
+*/
 /*
 $('section').on('click', '.feelings', function(){
 	$(this).toggleClass('changeicon');
@@ -104,9 +128,9 @@ function storePosts(posts){
     posts = JSON.stringify(posts);
     console.log('json: ' + posts);
 
+
     // store the string
     localStorage.posts = posts;
-	
 }
 
 
